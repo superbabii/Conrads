f042a76dd13878e497b5d86eabe6b6ae65d279f1
superbabii <nzrbabii@gmail.com>
2024-04-02 10:19:48 +0300

    reinit

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,8 +1,13 @@
+import * as React from 'react';
 import { Metadata } from "next";
 import Breadcrumb from "@/components/Breadcrumb";
 import Layout from "../layout";
 import { GetStaticProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { styled } from '@mui/material/styles';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
 
 type Props = {
     // Define the props here
@@ -13,13 +18,76 @@ export const metadata: Metadata = {
     description: "This is Product Page for Conrads Consult & Engineering",
 };
 
+interface StyledTabsProps {
+    children?: React.ReactNode;
+    value: number;
+    onChange: (event: React.SyntheticEvent, newValue: number) => void;
+}
+
+const StyledTabs = styled((props: StyledTabsProps) => (
+    <Tabs
+        {...props}
+        TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
+    />
+))({
+    '& .MuiTabs-indicator': {
+        display: 'flex',
+        justifyContent: 'center',
+        backgroundColor: 'transparent',
+    },
+    '& .MuiTabs-indicatorSpan': {
+        maxWidth: 40,
+        width: '100%',
+        backgroundColor: '#801022',
+    },
+});
+
+interface StyledTabProps {
+    label: string;
+}
+
+const StyledTab = styled((props: StyledTabProps) => (
+    <Tab disableRipple {...props} />
+))(({ theme }) => ({
+    textTransform: 'none',
+    fontWeight: theme.typography.fontWeightRegular,
+    fontSize: theme.typography.pxToRem(15),
+    marginRight: theme.spacing(1),
+    color: 'rgba(255, 255, 255, 0.7)',
+    '&.Mui-selected': {
+        color: '#fff',
+    },
+    '&.Mui-focusVisible': {
+        backgroundColor: 'rgba(128, 16, 34, 1)',
+    },
+}));
+
 const Products = () => {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+
     return (
         <Layout>
             <Breadcrumb
                 pageName="Products"
                 description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
             />
+            <div className="mx-auto max-w-7xl my-4 md:my-8 px-4 sm:py-4 lg:px-8">
+                <StyledTabs
+                    value={value}
+                    onChange={handleChange}
+                    aria-label="styled tabs example"
+                >
+                    <StyledTab label="Automotive" />
+                    <StyledTab label="AI" />
+                    <StyledTab label="Electronic" />
+                    <StyledTab label="Software" />
+                </StyledTabs>
+                <div className='text-offwhite p-5'>{value}</div>
+            </div>
         </Layout>
     );
 };
