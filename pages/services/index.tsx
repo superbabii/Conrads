b274bcfec983f0e9a9a638241e2d2ae9ea91eca0
superbabii <nzrbabii@gmail.com>
2024-04-02 05:19:42 +0300

    reinit

--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -60,31 +60,31 @@ const Services = () => {
                 pageName="Services"
                 description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
             />
-           {servicedata.map((items, i) => (
-    <div className="mx-auto max-w-7xl my-4 md:my-8 px-4 sm:py-4 lg:px-8" key={i}>
-        <div className={`grid grid-cols-1 md:grid-cols-2 gap-24 mb-12 ${i % 2 === 0 ? 'md:flex-row-reverse' : 'md:flex-row'}`}>
-            <div className={`relative text-container md:order-${i % 2 === 0 ? '2' : '1'}`}>
-                <h2 className="text-3xl lg:text-5xl pt-4 font-bold sm:leading-tight mt-5 text-center text-white">
-                    Our team believes you deserve <br /> only the best.
-                </h2>
-                <p className="text-lg font-medium text-center text-maingray pt-10">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br /> eiusmod tempor.
-                </p>
-            </div >
-            <div className={`flex flex-col justify-between p-5 transition duration-400 bg-primary rounded-lg card-hover ${i % 2 === 0 ? 'md:order-1' : 'md:order-2'}`}>
-                <div className='rounded-full flex justify-center p-3'>
-                    <Image src={items.imgSrc} alt={items.imgSrc} width={120} height={120} />
+            {servicedata.map((items, i) => (
+                <div className="mx-auto max-w-7xl my-4 md:my-8 px-4 sm:py-4 lg:px-8" key={i}>
+                    <div className={`grid grid-cols-1 md:grid-cols-2 gap-24 mb-12 ${i % 2 === 0 ? 'md:flex-row-reverse' : 'md:flex-row'}`}>
+                        <div className={`relative text-container md:order-${i % 2 === 0 ? '2' : '1'}`}>
+                            <h2 className="text-3xl lg:text-5xl pt-4 font-bold sm:leading-tight mt-5 text-center text-white">
+                                Our team believes you deserve <br /> only the best.
+                            </h2>
+                            <p className="text-lg font-medium text-center text-maingray pt-10">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br /> eiusmod tempor.
+                            </p>
+                        </div >
+                        <div className={`flex flex-col justify-between p-5 transition duration-400 bg-primary rounded-lg card-hover ${i % 2 === 0 ? 'md:order-1' : 'md:order-2'}`}>
+                            <div className='rounded-full flex justify-center p-3'>
+                                <Image src={items.imgSrc} alt={items.imgSrc} width={120} height={120} />
+                            </div>
+                            <h3 className="text-2xl font-semibold text-white text-center">
+                                {items.heading}
+                            </h3>
+                            <p className='text-base font-normal text-white text-center'>
+                                {items.subheading}
+                            </p>
+                        </div>
+                    </div>
                 </div>
-                <h3 className="text-2xl font-semibold text-white text-center">
-                    {items.heading}
-                </h3>
-                <p className='text-base font-normal text-white text-center'>
-                    {items.subheading}
-                </p>
-            </div>
-        </div>
-    </div>
-))}
+            ))}
 
 
         </Layout>
