8d3e7d4772f38682ca6bdd3e88e5d44741397c31
superbabii <nzrbabii@gmail.com>
2024-04-02 18:47:47 +0300

    reinit

--- components/Services/index.tsx
+++ components/Services/index.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface servicedata {
     imgSrc: string;
@@ -42,34 +43,35 @@ const servicedata: servicedata[] = [
 
 const Services = () => {
     return (
-        <div>
-            <div className='mx-auto max-w-7xl mt-16 px-6 mb-20 relative'>
-                <div className="radial-bgone hidden lg:block" />
-                <div className='text-center mb-14'>
-                    <h3 className='text-offwhite text-3xl md:text-5xl font-bold mb-3'>
-                        Services
-                    </h3>
-                    <p className='text-white md:text-lg font-normal leading-8'>
-                        At Conrads Consult & Engineering, we provide tailored outsourcing solutions in mechanical design, PCB development, and software integration, leveraging advanced simulations to optimize business operations.
-                    </p>
-                </div>
+        <div className='mx-auto max-w-7xl mt-16 px-6 mb-20 relative'>
+            <div className="radial-bgone hidden lg:block" />
+            <div className='text-center mb-14'>
+                <h3 className='text-offwhite text-3xl md:text-5xl font-bold mb-3'>
+                    Services
+                </h3>
+                <p className='text-white md:text-lg font-normal leading-8'>
+                    At Conrads Consult & Engineering, we provide tailored outsourcing solutions in mechanical design, PCB development, and software integration, leveraging advanced simulations to optimize business operations.
+                </p>
+            </div>
 
-                <div className='grid md:grid-cols-2 lg:grid-cols-3 lg:gap-12 gap-y-20 mt-32'>
-                    {servicedata.map((items, i) => (
-                        <div className="flex shadow-lg hover:shadow-[#c4243e] flex-col justify-between p-5 transition duration-400 bg-primary rounded-lg card-hover" key={i}>
-                            <div className='rounded-full flex justify-center p-3'>
-                                <Image src={items.imgSrc} alt={items.imgSrc} width={120} height={120} />
-                            </div>
-                            <h3 className="text-2xl font-semibold text-white text-center">
-                                {items.heading}
-                            </h3>
-                            <p className='text-base font-normal text-white text-center'>
-                                {items.subheading}
-                            </p>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 lg:gap-12 gap-y-20 mt-32'>
+                {servicedata.map((items, index) => (
+                    <Link
+                        className="flex shadow-lg hover:shadow-[#c4243e] flex-col justify-between p-5 transition duration-400 bg-primary rounded-lg card-hover"
+                        href={`/services#service-${index}`}
+                        key={index}
+                    >
+                        <div className='rounded-full flex justify-center p-3'>
+                            <Image src={items.imgSrc} alt={items.imgSrc} width={120} height={120} />
                         </div>
-                    ))}
-                </div>
-
+                        <h3 className="text-2xl font-semibold text-white text-center">
+                            {items.heading}
+                        </h3>
+                        <p className='text-base font-normal text-white text-center'>
+                            {items.subheading}
+                        </p>
+                    </Link>
+                ))}
             </div>
         </div>
     )
--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -4,6 +4,8 @@ import Breadcrumb from "@/components/Breadcrumb";
 import Layout from "../layout";
 import { GetStaticProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 type Props = {
     // Define the props here
@@ -54,6 +56,24 @@ const servicedata: servicedata[] = [
 ]
 
 const Services = () => {
+
+    const router = useRouter();
+
+    const section = router.asPath.split('#')[1]?.split('-')[1];
+
+    useEffect(() => {
+        if (section) {
+            scrollToSection(section);
+        }
+    }, [section]);
+
+    const scrollToSection = (index: string) => {
+        const element = document.getElementById(`service-${index}`);
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        }
+    };
+
     return (
         <Layout>
             <Breadcrumb
@@ -61,10 +81,10 @@ const Services = () => {
                 description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
             />
             <div className="mx-auto max-w-7xl px-4 sm:py-4 lg:px-8 my-12">
-                {servicedata.map((items, i) => (
-                    <div className="mx-auto max-w-7xl mb-4 md:mb-8 px-4 sm:pb-4 lg:px-8" key={i}>
-                        <div className={`grid grid-cols-1 md:grid-cols-3 gap-4 ${i % 2 === 0 ? 'md:flex-row-reverse' : 'md:flex-row'}`}>
-                            <div className={`col-span-1 md:col-span-2 flex flex-col justify-center text-container md:order-${i % 2 === 0 ? '2' : '1'}`}>
+                {servicedata.map((items, index) => (
+                    <section id={`service-${index}`} className="mx-auto max-w-7xl mb-4 md:mb-8 px-4 sm:pb-4 lg:px-8" key={index}>
+                        <div className={`grid grid-cols-1 md:grid-cols-3 gap-4 ${index % 2 === 0 ? 'md:flex-row-reverse' : 'md:flex-row'}`}>
+                            <div className={`col-span-1 md:col-span-2 flex flex-col justify-center text-container md:order-${index % 2 === 0 ? '2' : '1'}`}>
                                 <h2 className="text-2xl lg:text-4xl font-bold sm:leading-tight text-center text-white">
                                     Our team believes you deserve <br /> only the best.
                                 </h2>
@@ -72,7 +92,7 @@ const Services = () => {
                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br /> eiusmod tempor.
                                 </p>
                             </div >
-                            <div className={`col-span-1 flex flex-col justify-center items-center p-5 transition duration-400 bg-primary rounded-lg opacity-40 ${i % 2 === 0 ? 'md:order-1' : 'md:order-2'}`}>
+                            <div className={`col-span-1 flex flex-col justify-center items-center p-5 transition duration-400 bg-primary rounded-lg opacity-40 ${index % 2 === 0 ? 'md:order-1' : 'md:order-2'}`}>
                                 <div className='rounded-full flex justify-center p-3'>
                                     <Image src={items.imgSrc} alt={items.imgSrc} width={120} height={120} />
                                 </div>
@@ -84,7 +104,7 @@ const Services = () => {
                                 </p>
                             </div>
                         </div>
-                    </div>
+                    </section>
                 ))}
             </div>
         </Layout>
