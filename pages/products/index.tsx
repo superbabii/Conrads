147bff8de80ff0ce85f8be20fdbab402f896c321
superbabii <nzrbabii@gmail.com>
2024-04-03 11:21:31 +0300

    reinit

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -7,7 +7,6 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { styled } from '@mui/material/styles';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
 
 type Props = {
     // Define the props here
@@ -65,75 +64,69 @@ const StyledTab = styled((props: StyledTabProps) => (
 interface productdata {
     imgSrc: string;
     heading: string;
-    subheading: string;
+    price: number;
 }
 
 const productdata: productdata[] = [
     {
         imgSrc: '/images/Products/AI.jpg',
         heading: 'Products-exm1',
-        subheading: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        price: 1000,
     },
     {
         imgSrc: '/images/Products/simulation.jpg',
         heading: 'Products-exm1',
-        subheading: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry and this',
+        price: 100,
     },
 ]
 interface ElectronicsAndSoftwareCardProps {
     productName: string;
-    productDescription: string;
     imageUrl: string;
+    price: number;
+    discount?: number;
 }
 
-const ElectronicsAndSoftwareCard: React.FC<ElectronicsAndSoftwareCardProps> = ({ productName, productDescription, imageUrl }) => {
+const ElectronicsAndSoftwareCard: React.FC<ElectronicsAndSoftwareCardProps> = ({ productName, imageUrl, price, discount }) => {
     return (
         <div className="relative group">
             {/* Gradient overlay */}
-            <div className="absolute transition opacity-40 -inset-1 bg-gradient-to-r from-[#592693] to-[#96225f] blur duration-400 group-hover:opacity-100 group-hover:duration-200" />
+            {/* <div className="absolute transition opacity-40 -inset-1 bg-gradient-to-r from-[#592693] to-[#96225f] blur duration-400 group-hover:opacity-100 group-hover:duration-200" /> */}
 
-            {/* Product image */}
             <div
-                className='relative'
-                onMouseOver={(e) => {
-                    const highlightElement = e.currentTarget.querySelector('.highlight') as HTMLElement | null;
-                    if (highlightElement) {
-                        highlightElement.style.opacity = '1';
-                    }
+                className="relative space-y-6 leading-none rounded-lg highlight"
+                style={{
+                    backgroundImage: `url(${imageUrl})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat',
+                    height: '240px',
+                    opacity: 1,
                 }}
-                onMouseOut={(e) => {
-                    const highlightElement = e.currentTarget.querySelector('.highlight') as HTMLElement | null;
-                    if (highlightElement) {
-                        highlightElement.style.opacity = '0.7';
-                    }
-                }}
-            >
-                <div
-                    className="relative space-y-6 leading-none rounded-lg ring-1 ring-gray-900/5 highlight"
-                    style={{
-                        backgroundImage: `url(${imageUrl})`,
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        backgroundRepeat: 'no-repeat',
-                        height: '240px',
-                        opacity: 0.7,
-                        transition: 'opacity 0.4s ease-in-out',
-                    }}
-                ></div>
+            ></div>
 
-                {/* Product information */}
-                <div className="absolute bottom-0 left-0 right-0 p-2">
-                    <h3 className="text-md text-white border p-1 border-white rounded-md z-10 inline-block">
-                        {productName}
-                    </h3>
-                    <p className="leading-normal text-white text-lg py-3">
-                        {productDescription}
-                    </p>
+            {/* Product information */}
+            <div className="absolute bottom-0 left-0 right-0 p-2 bg-gray-800 bg-opacity-80">
+                <h3 className="text-md text-white border p-1 border-white rounded-md z-10 inline-block">
+                    {productName}
+                </h3>
+                <div className="flex justify-between items-center">
+                    <span className="text-white text-lg">
+                        Price: ${price}
+                    </span>
+                    {discount && (
+                        <span className="text-green-500 text-lg">
+                            Discount: {discount}%
+                        </span>
+                    )}
                 </div>
+                <button className="mt-2 bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded inline-block">
+                    Add to Cart
+                </button>
             </div>
         </div>
     );
 }
+
 
 
 const Products = () => {
@@ -150,30 +143,37 @@ const Products = () => {
                 description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
                 backgroundImageUrl="/images/Breadcrumb/background.png"
             />
-            <div className="mx-auto max-w-7xl px-4 sm:py-4 lg:px-8 my-12">
-                <StyledTabs
-                    value={value}
-                    onChange={handleChange}
-                    aria-label="styled tabs example"
-                >
-                    <StyledTab label="All" />
-                    <StyledTab label="Automotive" />
-                    <StyledTab label="AI" />
-                    <StyledTab label="Electronic" />
-                    <StyledTab label="Software" />
-                </StyledTabs>
-                <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-12 gap-y-20 mt-32" >
-                    {productdata.map((items, index) => (
-                        <ul className="space-y-8" key={index}>
-                            <li className="text-sm leading-6">
-                                <ElectronicsAndSoftwareCard
-                                    productName={items.heading}
-                                    productDescription={items.subheading}
-                                    imageUrl={items.imgSrc}
-                                />
-                            </li>
-                        </ul>
-                    ))}
+            <div className='flex mx-auto max-w-7xl px-4 sm:py-4 lg:px-8 my-12'>
+                <div className="space-x-8 mr-32">
+                    <h2 className="text-lg font-medium text-white">Popular Products</h2>
+                    {/* Render your popular products list here */}
+                </div>
+                <div>
+                    <StyledTabs
+                        value={value}
+                        onChange={handleChange}
+                        aria-label="styled tabs example"
+                    >
+                        <StyledTab label="All" />
+                        <StyledTab label="Automotive" />
+                        <StyledTab label="AI" />
+                        <StyledTab label="Electronic" />
+                        <StyledTab label="Software" />
+                    </StyledTabs>
+                    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-12 gap-y-20 mt-32" >
+                        {productdata.map((items, index) => (
+                            <ul className="space-y-8" key={index}>
+                                <li className="text-sm leading-6">
+                                    <ElectronicsAndSoftwareCard
+                                        productName={items.heading}
+                                        price={items.price}
+                                        imageUrl={items.imgSrc}
+                                    />
+                                </li>
+                            </ul>
+                        ))}
+                    </div>
+
                 </div>
             </div>
         </Layout>
