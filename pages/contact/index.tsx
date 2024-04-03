b96789b5c9520bc34523acaa9e23210799d816b5
superbabii <nzrbabii@gmail.com>
2024-04-04 00:14:13 +0300

    reinit

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -27,8 +27,7 @@ const Contact = () => {
                 description="Don&apos;t hesitate to connect with us. Our team is readily available and eager to assist you with any inquiries or assistance you may need. Reach out to us today; we&apos;re here to help!"
                 backgroundImageUrl="/images/Breadcrumb/background.png"
             />
-            <div className="mx-auto max-w-7xl px-4 sm:py-4 lg:px-8 mt-12">
-                <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-4 mb-12'>
+                <div className='mx-auto max-w-7xl px-4 sm:py-4 lg:px-8 grid md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-4 my-12'>
                     <div className="flex shadow-lg hover:shadow-[#c4243e] flex-col justify-between p-5 transition duration-400 bg-transparent rounded-lg contact-card">
                         <div className='rounded-full flex justify-center p-3'>
                             <HomeIcon style={{ color: '#801022', width: 100, height: 100 }} />
@@ -77,54 +76,56 @@ const Contact = () => {
                         </p>
                     </div>
                 </div>
-                <div className="py-4 lg:w-5/12">
-                    <div
-                        className="rounded-lg bg-transparent shadow-lg contact-card px-16 py-12"
-                        data-wow-delay=".15s"
-                    >
-                        <h2 className="mb-8 text-2xl font-bold text-white sm:text-3xl lg:text-2xl xl:text-3xl">
-                            Free Consultation
-                        </h2>
-                        <form>
-                            <div className="-mx-4">
-                                <div className="w-full px-4">
-                                    <div className="mb-8">
-                                        <input
-                                            type="text"
-                                            placeholder="Enter your name"
-                                            className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 outline-none focus:border-primary"
-                                        />
+                <div className="red-band"></div>
+                <div className="mx-auto max-w-7xl px-4 sm:py-4 lg:px-8">
+                    <div className="py-4 lg:w-5/12">
+                        <div
+                            className="rounded-lg bg-transparent shadow-lg free-consultation px-16 py-12"
+                            data-wow-delay=".15s"
+                        >
+                            <h2 className="mb-8 text-2xl font-bold text-white sm:text-3xl lg:text-2xl xl:text-3xl">
+                                Free Consultation
+                            </h2>
+                            <form>
+                                <div className="-mx-4">
+                                    <div className="w-full px-4">
+                                        <div className="mb-8">
+                                            <input
+                                                type="text"
+                                                placeholder="Enter your name"
+                                                className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 outline-none focus:border-primary"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="w-full px-4">
+                                        <div className="mb-8">
+                                            <input
+                                                type="email"
+                                                placeholder="Enter your email"
+                                                className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 outline-none focus:border-primary"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="w-full px-4">
+                                        <div className="mb-8">
+                                            <textarea
+                                                name="message"
+                                                rows={5}
+                                                placeholder="Enter your Message"
+                                                className="border-stroke w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 outline-none focus:border-primary"
+                                            ></textarea>
+                                        </div>
+                                    </div>
+                                    <div className="w-full px-4">
+                                        <button className='text-lg text-white mainbutton'>
+                                            Free Consultant <KeyboardDoubleArrowRightIcon />
+                                        </button>
                                     </div>
                                 </div>
-                                <div className="w-full px-4">
-                                    <div className="mb-8">
-                                        <input
-                                            type="email"
-                                            placeholder="Enter your email"
-                                            className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 outline-none focus:border-primary"
-                                        />
-                                    </div>
-                                </div>
-                                <div className="w-full px-4">
-                                    <div className="mb-8">
-                                        <textarea
-                                            name="message"
-                                            rows={5}
-                                            placeholder="Enter your Message"
-                                            className="border-stroke w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 outline-none focus:border-primary"
-                                        ></textarea>
-                                    </div>
-                                </div>
-                                <div className="w-full px-4">
-                                    <button className='text-lg text-white mainbutton'>
-                                        Free Consultant <KeyboardDoubleArrowRightIcon />
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
         </Layout>
     );
 };
